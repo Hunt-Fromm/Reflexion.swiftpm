058262ddb614a0ac45b8b0d4b254d5894be67204
Hunter Frommelt <fro512131@d204.lths.net>
2025-04-16 13:06:17 -0500

    Started drafting ContentView appearance and sortWorkouts function

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var workouts: [Workout] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        // Navigation Stack to which toolbar is applied
+        NavigationStack {
+            
+            VStack {
+                Text("Workout Log")
+                    .font(.custom("Arial Bold", size: 40))
+                
+                // Scrollview holding this page's data
+                ScrollView {
+                    
+                    // This TextView is to be changed later
+                    Text("April 2025")
+                        .frame(width: UIScreen.main.bounds.width - 40, alignment: .leading)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    
+                    
+                } // End ScrollView
+            } // End VStack
+        } // End NavStack
+        
+        
+    }
+    
+    // Bare bones of sortWorkouts function
+    func sortWorkouts() {
+        var temp: Workout
+        
+        for i in 0..<workouts.count {
+            
+            for j in i..<workouts.count {
+                
+            }
         }
     }
 }
