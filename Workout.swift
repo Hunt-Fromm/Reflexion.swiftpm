435d7063b97cc491d70fe43ac2c0501dde8faecc
Hunter Frommelt <fro512131@d204.lths.net>
2025-04-16 12:49:45 -0500

    Created Workout Object and its Initializer

--- Workout.swift
+++ Workout.swift
@@ -8,6 +8,37 @@
 import SwiftUI
 
 struct Workout: Hashable {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    
+    var type: String
     
     var dayOfTheWeek: String
+    var date: Int
+    var month: Int
+    var year: Int
+    
+    var hours: Int
+    var minutes: Int
+    
+    var mood: Int
+    var energy: Int
+    var reflection: String
+    
+    /// Creates a Workout Object, initializing the values of all instance variables
+    init(type: String, dayOfTheWeek: String, date: Int, month: Int, year: Int, hours: Int, minutes: Int, mood: Int, energy: Int, reflection: String) {
+        self.type = type
+        self.dayOfTheWeek = dayOfTheWeek
+        self.date = date
+        self.month = month
+        self.year = year
+        self.hours = hours
+        self.minutes = minutes
+        self.mood = mood
+        self.energy = energy
+        self.reflection = reflection
+    }
+    
+    
+    
+    
 }
